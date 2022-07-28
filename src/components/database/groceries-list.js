ae70fe78868261d1e4f4f5d8d43f899572a18fd4
Victoria Ok <victoriaok@INTERNMBP-Victoria-Ok.local>
2022-07-28 19:38:08 -0400

    add images to product cards

---
+++ src/components/database/groceries-list.js
@@ -0,0 +1,33 @@
+import milk from "./assets/milk.jpg"
+import bread from "./assets/bread.jpg"
+import water from "./assets/water.jpg"
+import salad from "./assets/salad.jpg"
+import sourpatch from "./assets/sourpatch.jpg"
+
+export default [
+    {
+        "name": "Milk",
+        "cost": 3.75,
+        "image": milk
+    },
+    {
+        "name": "Bread",
+        "cost": 2.00,
+        "image": bread
+    },
+    {
+        "name": "Water",
+        "cost": 1.00,
+        "image": water
+    },
+    {
+        "name": "Salad",
+        "cost": 5.00,
+        "image": salad
+    },
+    {
+        "name": "Candy",
+        "cost": 7.00,
+        "image": sourpatch
+    },
+  ];
---
+++ src/components/database/snacks-list.js
@@ -0,0 +1,39 @@
+import sourpatch from "./assets/sourpatch.jpg"
+import cheetos from "./assets/cheetos.jpg"
+import snickers from "./assets/snickers.jpg"
+import funyuns from "./assets/funyuns.jpg"
+import lays from "./assets/lays.jpg"
+import pringles from "./assets/pringles.jpg"
+
+export default [
+    {
+        "name": "Sour Patch Kids",
+        "cost": 3.75,
+        "image": sourpatch
+    },
+    {
+        "name": "Flaming Hot Cheetos",
+        "cost": 2.00,
+        "image": cheetos
+    },
+    {
+        "name": "Snickers Bar",
+        "cost": 1.00,
+        "image": snickers
+    },
+    {
+        "name": "Funyuns",
+        "cost": 5.00,
+        "image": funyuns
+    },
+    {
+        "name": "Lays Classic",
+        "cost": 7.00,
+        "image": lays
+    },
+    {
+        "name": "Pringles Original",
+        "cost": 5.00,
+        "image": pringles
+    },
+  ];
