278395f91bd6a6211a9cdb9225e7905169fd0e44
miriamshamsrainey <mshamsrainey@gmail.com>
2022-07-28 21:12:39 -0400

    Speechly fix in index.js

--- src/index.js
+++ src/index.js
@@ -9,19 +9,12 @@ import { SpeechProvider } from "@speechly/react-client";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// Speechly setup
-ReactDOM.render(
-  <React.StrictMode>
     <SpeechProvider appId="a3a4ebec-c7fc-40c9-b370-e3b819c6d15f">
       <App />
     </SpeechProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
